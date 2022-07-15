43604f7205e2bc874443ffab4e2d928eb05d02f2
Xiang Wu <xiang.wu@hotmail.de>
2022-07-15 13:03:01 +0200

    replace the hard code routes with empty component

--- src/App.js
+++ src/App.js
@@ -25,44 +25,44 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-              Sidebar 1
+              <Sidebar/>
             </div>):(
             <div className='w-0 dark:bg-secondary-dark-bg'>
-              Sidebar w-0
+              <Sidebar/> w-0
             </div>)}
             
             <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72':'flex-2'}`}>
               <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg w-full'>
-                navbar
+                <Navbar/>
               </div>
             </div>
 
             <div>
               <Routes>
-                 {/* dashboard  */}
-                 <Route path="/" element="Ecommerce" />
-                <Route path="/ecommerce" element="Ecommerce" />
+                {/* dashboard  */}
+                <Route path="/" element={(<Ecommerce />)} />
+                <Route path="/ecommerce" element={(<Ecommerce />)} />
 
                 {/* pages  */}
-                <Route path="/orders" element="Orders" />
-                <Route path="/employees" element="Employees" />
-                <Route path="/customers" element="Customers" />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
 
                 {/* apps  */}
-                <Route path="/kanban" element="Kanban" />
-                <Route path="/editor" element="Editor" />
-                <Route path="/calendar" element="Calendar" />
-                <Route path="/color-picker" element="ColorPicker" />
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
 
                 {/* charts  */}
-                <Route path="/line" element="Line" />
-                <Route path="/area" element="Area" />
-                <Route path="/bar" element="Bar" />
-                <Route path="/pie" element="Pie" />
-                <Route path="/financial" element="Financial" />
-                <Route path="/color-mapping" element="ColorMapping" />
-                <Route path="/pyramid" element="Pyramid" />
-                <Route path="/stacked" element="Stacked" />
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
               </Routes>
             </div>
         </div>
